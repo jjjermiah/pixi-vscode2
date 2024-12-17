6361422c3dda7096159429e992cc97dac0c31335
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-12-17 09:24:59 -0500

    refactor: Rename logging functions for consistency and clarity

--- src/common/logging.ts
+++ src/common/logging.ts
@@ -41,55 +41,55 @@ type Arguments = unknown[];
  * @param {LogOutputChannel} channel - The output channel to which logs will be written.
  */
 class OutputChannelLogger {
-    constructor(private readonly channel: LogOutputChannel) {}
+	constructor(private readonly channel: LogOutputChannel) { }
 
-    public traceLog(...data: Arguments): void {
-        this.channel.appendLine(util.format(...data));
-    }
+	public traceLog(...data: Arguments): void {
+		this.channel.appendLine(util.format(...data));
+	}
 
-    public traceError(...data: Arguments): void {
-        this.channel.error(util.format(...data));
-    }
+	public traceError(...data: Arguments): void {
+		this.channel.error(util.format(...data));
+	}
 
-    public traceWarn(...data: Arguments): void {
-        this.channel.warn(util.format(...data));
-    }
+	public traceWarn(...data: Arguments): void {
+		this.channel.warn(util.format(...data));
+	}
 
-    public traceInfo(...data: Arguments): void {
-        this.channel.info(util.format(...data));
-    }
+	public traceInfo(...data: Arguments): void {
+		this.channel.info(util.format(...data));
+	}
 
-    public traceVerbose(...data: Arguments): void {
-        this.channel.debug(util.format(...data));
-    }
+	public traceDebug(...data: Arguments): void {
+		this.channel.debug(util.format(...data));
+	}
 }
 
 let channel: OutputChannelLogger | undefined;
 export function registerLogger(logChannel: LogOutputChannel): Disposable {
-    channel = new OutputChannelLogger(logChannel);
-    return {
-        dispose: () => {
-            channel = undefined;
-        },
-    };
+	channel = new OutputChannelLogger(logChannel);
+	return {
+		dispose: () => {
+			channel = undefined;
+		},
+	};
 }
 
-export function traceLog(...args: Arguments): void {
-    channel?.traceLog(...args);
+export function log(...args: Arguments): void {
+	channel?.traceLog(...args);
 }
 
-export function traceError(...args: Arguments): void {
-    channel?.traceError(...args);
+export function error(...args: Arguments): void {
+	channel?.traceError(...args);
 }
 
-export function traceWarn(...args: Arguments): void {
-    channel?.traceWarn(...args);
+export function warn(...args: Arguments): void {
+	channel?.traceWarn(...args);
 }
 
-export function traceInfo(...args: Arguments): void {
-    channel?.traceInfo(...args);
+export function info(...args: Arguments): void {
+	channel?.traceInfo(...args);
 }
 
-export function traceVerbose(...args: Arguments): void {
-    channel?.traceVerbose(...args);
+export function debug(...args: Arguments): void {
+	channel?.traceDebug(...args);
 }
